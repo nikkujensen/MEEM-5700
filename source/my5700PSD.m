ce96cfff3f055702f4307efc34f6d449e5b9c9c9
Nick Jensen <55917432+IloveJesusmyKing@users.noreply.github.com>
2020-09-05 21:00:30 -0400

    Create my5700PSD.m
    Started it but need to verify

---
+++ source/my5700PSD.m
@@ -0,0 +1,28 @@
+function  my5700PSD(time,sig)
+%MY5700FFT Summary of this function goes here
+%   Detailed explanation goes here
+
+%% calculate some parameters based on the inputs
+T = time(2) - time(1);          % (sec)     sampling period
+fs = 1/T;                       % (Hz)      sampling frequency
+N = length(sig);                % (null)    block size
+fNyq = fs/2;                    % (Hz)      nyquist frequency
+f = linspace(0,fNyq,N/2+1);     % (Hz)      frequency vector. one added to include zero
+
+%% calculate the transform
+Y = fft(sig);                   % matlab fft command
+Y = Y.*conj(Y);                 % convert to power of the signal
+Y2 = abs(Y/N);                  % two-sided spectrum. normalize for further calcs???
+Y1 = Y2(1:N/2+1);               % single sided spectrum
+Y1(2:end-1) = 2*Y1(2:end-1);    % correction to include full signal strength from the other side???          
+
+%% plot it!
+% figure
+semilogx(f,mag2db(abs(Y1)),"linewidth",2)
+xlabel("Frequency (Hz)")
+ylabel("Mag dB")
+title("Power Spectral Density (PSD)")
+grid on
+
+end
+
