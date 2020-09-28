31f6f8702a78ff1889b51b4de85f559c1dd6518b
Nick Jensen <55917432+IloveJesusmyKing@users.noreply.github.com>
2020-09-28 19:59:40 -0400

    Update my5700PSD.m
    Implemented the energy correction factor

--- source/my5700PSD.m
+++ source/my5700PSD.m
@@ -1,13 +1,16 @@
-function  [Y1PSD, f] = my5700PSD(time,sig)
+function  [Y1PSD, f] = my5700PSD(time,sig,win)
 %MY5700PSD Summary of this function goes here
 %   Detailed explanation goes here
 
 %% utilize the custom FFT function
 [Y1S, f] = my5700FFT(time,sig);
 
+%% calculate the window correction factor
+ECF = 1/rms(win);               %           energy correction factor
+
 %% perform PSD calcs
 delta_f = f(2)-f(1);            % (Hz)      frequency resolution
-Y1A = abs(Y1S);                 %           get the autopower
+Y1A = ECF*abs(Y1S);             %           get the autopower
 Y1PSD = Y1A./delta_f;           %           power spectral density
 
 end
