bec4b64312ccccaafe7b6c8f893675311c08b9a9
Nick Jensen <55917432+IloveJesusmyKing@users.noreply.github.com>
2020-10-12 15:06:52 -0400

    Update my5700FFT.m
    Corrected the following
    
    Correct 2x multiplier on FFT. When values at f=0,fnyq are considered, you don't multiply these by 2 for scaling. See the MATLAB example from FFT documentation below

--- source/my5700FFT.m
+++ source/my5700FFT.m
@@ -13,9 +13,11 @@ ACF = 1/mean(win);              %           amplitude correction factor
 if ~rem(N,2)
     % even
     len = N/2+1;
+	flg = 1;
 else
     % odd
     len = N/2+0.5;
+	flg = 0;
 end
 
 % apply the window before the freq domain
@@ -24,7 +26,16 @@ sigW = win.*sig;
 %% calculate the transform
 Y2  = fft(sigW);                % matlab fft command - double sided
 Y1  = Y2(1:len);                % get the single sided
-Y1S = ACF*2/N.*Y1;              % scale the single sided to have full spectrum power and a set period (using N blocks)
+
+% based on the signal length, determine how to scale (don't double multiply for 0 or fNyq if it is not present on the negative frequency side)
+if flg
+	Y1S = Y1;								% get all the data at first
+	Y1S(2:end) = ACF*2/N.*Y1S(2:end);   	% scale the single sided to have full spectrum power and a set period (using N blocks)
+else
+	Y1S = Y1;								% get all the data at first
+	Y1S(2:end-1) = ACF*2/N.*Y1S(2:end-1);   % scale the single sided to have full spectrum power and a set period (using N blocks)
+end
+
 f = linspace(0,fNyq,len);       % (Hz)  frequency vector
 
 end
