e4fa51a4d30ac8dfd1631d13692a584eebb97359
Nick Jensen <55917432+IloveJesusmyKing@users.noreply.github.com>
2020-10-12 15:33:45 -0400

    Update my5700Ave_LinWeight.m
    Condensed this code to remove loops and utilize the reshape and mean function from MATLAB

--- source/my5700Ave_LinWeight.m
+++ source/my5700Ave_LinWeight.m
@@ -10,25 +10,31 @@ dataChop = data(len-rmnd+1:end);% get the chopped off data
 newLen = length(newData);       % get the new data length
 blcks = newLen/N;               % get the number of blocks
 
-% now chunk up the data
-k = 1;                      % counter
-y = zeros(blcks,N);    % initialize
-for i = 1:blcks
-    y(i,:) = newData(k:k+N-1);          % get the next block
-    k = k + N;                          % shift the block
-end
+% reshape the data to the block sizes
+y = reshape(newData,blcks,N);	% row is the run. column is the data
 
-% now average up each of the data points
-yAve = zeros(1,N);     % initialize
-for k = 1:N
+% average the data
+yAve = mean(y,1);
+
+% % now chunk up the data
+% k = 1;                      % counter
+% y = zeros(blcks,N);    % initialize
+% for i = 1:blcks
+    % y(i,:) = newData(k:k+N-1);          % get the next block
+    % k = k + N;                          % shift the block
+% end
+
+% % now average up each of the data points
+% yAve = zeros(1,N);     % initialize
+% for k = 1:N
     
-    % check if the chopped data can be averaged in (e.g. does it have data
-    % in that index)
-    if k<=rmnd
-        yAve(k) = mean([y(:,k);dataChop(k)]);   % average them with the chopped data
-    else
-        yAve(k) = mean(y(:,k));                 % average them without the chopped data
-    end
-end
+    % % check if the chopped data can be averaged in (e.g. does it have data
+    % % in that index)
+    % if k<=rmnd
+        % yAve(k) = mean([y(:,k);dataChop(k)]);   % average them with the chopped data
+    % else
+        % yAve(k) = mean(y(:,k));                 % average them without the chopped data
+    % end
+% end
 
 end
