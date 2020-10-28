1f22a35858acdb5a0a96efd83b47edf3a84583db
Nick Jensen <55917432+IloveJesusmyKing@users.noreply.github.com>
2020-10-28 15:59:46 -0400

    Updated Trim reshape to split these two steps

--- source/my5700Trim.m
+++ source/my5700Trim.m
@@ -1,10 +1,10 @@
-function yNew = my5700TrimReshape(y,N)
+function yNew = my5700Trim(y,N)
 % function used to trim data and reshape for the chosen block size. Data
 % will be outputted with columns being blocks and rows being the data.
 
 len  = length(y);               % data length
 rmnd = rem(len,N);              % get the remainder
-yTemp = y(1:len-rmnd);          % get data that can be split up evenly with the given block size
-yNew = reshape(yTemp,N,[]);     % reshape the data
+yNew = y(1:len-rmnd);           % get data that can be split up evenly with the given block size
+% yNew = reshape(yTemp,N,[]);     % reshape the data
 
 end
